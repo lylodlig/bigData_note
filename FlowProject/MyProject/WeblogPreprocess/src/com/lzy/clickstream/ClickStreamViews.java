3868798ca25eea43890559c7a2094f1efb8c54f5
tienal_lzy <lzy209312269>
2017-09-20 18:56:31 +0800

    update

--- FlowProject/MyProject/WeblogPreprocess/src/com/lzy/clickstream/ClickStreamViews.java
+++ FlowProject/MyProject/WeblogPreprocess/src/com/lzy/clickstream/ClickStreamViews.java
@@ -1,11 +1,23 @@
 package com.lzy.clickstream;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections.comparators.ComparableComparator;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import com.lzy.main.WeblogBean;
 
@@ -44,6 +56,68 @@ public class ClickStreamViews {
 				context.write(k, bean);
 
 		}
+	}
+
+	static class ClickStreamViewsReducer extends Reducer<Text, WeblogBean, NullWritable, Text> {
+		@Override
+		protected void reduce(Text key, Iterable<WeblogBean> values,
+				Reducer<Text, WeblogBean, NullWritable, Text>.Context arg2) throws IOException, InterruptedException {
+			// 把结果放到集合中
+			ArrayList<WeblogBean> beans = new ArrayList<>();
+			for (WeblogBean weblogBean : values) {
+				WeblogBean bean = new WeblogBean();
+				try {
+					BeanUtils.copyProperties(bean, weblogBean);
+					beans.add(bean);
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			// 按照时间顺序排序
+			Collections.sort(beans, new Comparator<WeblogBean>() {
+
+				@Override
+				public int compare(WeblogBean b1, WeblogBean b2) {
+					if (b1.time_local == null || b2.time_local == null) {
+						return 0;
+					}
+					try {
+						Date date1 = toDate(b1.time_local);
+						Date date2 = toDate(b1.time_local);
+						return date1.compareTo(date2);
+					} catch (ParseException e) {
+						e.printStackTrace();
+						return 0;
+					}
+				}
+			});
+
+			int step = 1;
+			String session = UUID.randomUUID().toString();
+			for (int i = 0; i < beans.size(); i++) {
+				//只有一条数据直接输出
+				if (beans.size() == 1) {
+
+				}
+			}
+		}
+
+		private Date toDate(String timeStr) throws ParseException {
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			return df.parse(timeStr);
+		}
+
+		private long timeDiff(String time1, String time2) throws ParseException {
+			Date d1 = toDate(time1);
+			Date d2 = toDate(time2);
+			return d1.getTime() - d2.getTime();
+
+		}
+
 	}
 
 }
